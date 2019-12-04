1e682f85e9a51f8751fb7a7cf75db25968a42c2c
eno <eno@open-lab.org>
2019-12-04 12:49:37 +0800

    Implements path separators

--- lib/simple/cli/runner.rb
+++ lib/simple/cli/runner.rb
@@ -24,9 +24,25 @@ module Simple::CLI
     def extract_flags!(args)
       flags = {}
 
+      had_path_separator = false
+
       args.reject! do |arg|
+        # path separator? When encountering "--" all following arguments are
+        # considered not flags.
+        #
+        # We set the had_path_separator flag (to remember that we have seen it,
+        # but then `next true` regardless because we need to remove the argument
+        # from the +args+ array.
+        if arg == "--"
+          had_path_separator = true
+          next true
+        end
+        break if had_path_separator
+
+        # Doesn't look like an argument?
         next false unless arg =~ /^--(no-)?([^=]+)(=(.+))?/
 
+        # Extract the flag, and its value (false, true, or a String)
         flag_name = $2.tr("-", "_")
         flag_name = "no_#{flag_name}" if $4 && $1
         value = $4 || ($1 ? false : true)
